a3dceca809ee70ee1a07d50ffd55a3fc4b981674
19013309 <19013309@LAPTOP-LJ9Q2JM8>
2020-08-18 10:10:12 +0800

    Created Appointment class

--- C206_CaseStudy/src/Appointment.java
+++ C206_CaseStudy/src/Appointment.java
@@ -1,4 +1,54 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Appointment {
+	//Jiawei
+	private LocalDate dateOfAppointment;
+	private LocalTime timeOfAppointment;
+	private String DesignerName;
+	private String address;
+	
+	public Appointment(LocalDate dateOfAppointment, LocalTime timeOfAppointment, String designerName, String address) {
+		this.dateOfAppointment = dateOfAppointment;
+		this.timeOfAppointment = timeOfAppointment;
+		DesignerName = designerName;
+		this.address = address;
+	}
+
+	public LocalDate getDateOfAppointment() {
+		return dateOfAppointment;
+	}
+
+	public void setDateOfAppointment(LocalDate dateOfAppointment) {
+		this.dateOfAppointment = dateOfAppointment;
+	}
+
+	public LocalTime getTimeOfAppointment() {
+		return timeOfAppointment;
+	}
+
+	public void setTimeOfAppointment(LocalTime timeOfAppointment) {
+		this.timeOfAppointment = timeOfAppointment;
+	}
+
+	public String getDesignerName() {
+		return DesignerName;
+	}
+
+	public void setDesignerName(String designerName) {
+		DesignerName = designerName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+	
 	
 }
