c28cb6b1a85c6d87409befcc6d220e18b222095d
19013309 <19013309@LAPTOP-LJ9Q2JM8>
2020-08-18 10:47:02 +0800

    done options

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -4,7 +4,7 @@ public class C206_CaseStudy {
 	private static final int OPTION_QUIT = 4;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		//hello people :D
 		//jiawei
 		int option = 0;
