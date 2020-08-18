c6ee49ce355727dd0d0a2ac45f16ceebee31df8b
19013309 <19013309@LAPTOP-LJ9Q2JM8>
2020-08-18 10:26:51 +0800

    Update Menues

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,11 +1,54 @@
 
 public class C206_CaseStudy {
 
+	private static final int OPTION_QUIT = 4;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
 		//hello people :D
+		//jiawei
+		int option = 0;
+		//jiawei
+		while (option != OPTION_QUIT) {
+			menu();
+			
+		}
+		
+	}
+	
+	//jiawei
+	public static void setHeader(String header) {
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
+	}
+	
+	//jiawei
+	public static void menu() {
+		C206_CaseStudy.setHeader("Renovation ACE APP");
+		System.out.println("1. Register an Account");
+		System.out.println("2. Login as Customer");
+		System.out.println("3. Login as Admin");
+		System.out.println("4. Quit");
+		Helper.line(80, "-");
+
+	}
+	
+	//jiawei
+	private static void CustomerMenu() {
+		C206_CaseStudy.setHeader("Customer Home Page");
+		System.out.println("1. Camcorder");
+		System.out.println("2. Chromebook");
+	}
+	
+	//Jiawei
+	private static void AdminMenue() {
+		C206_CaseStudy.setHeader("Admin Home Page");
+		System.out.println("1. Manage Customer");
+		System.out.println("2. Manage Package");
+		System.out.println("3. Manage Request for Quotation");
+		System.out.println("4. Manage Quotation");
+		System.out.println("5. Manage Appointment");
 	}
 
 }
