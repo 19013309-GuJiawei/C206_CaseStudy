dfa3055b3b5f0cdf679e903adf23aff7fad7a7fc
19013309 <19013309@LAPTOP-LJ9Q2JM8>
2020-08-18 09:54:26 +0800

    Created a new class called Account and Apointment.

---
+++ C206_CaseStudy/src/Account.java
@@ -0,0 +1,31 @@
+
+public class Account extends C206_CaseStudy {
+	//Jiawei
+	private String Username;
+	private String password;
+	
+	public Account(String username, String password) {
+		this.Username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return Username;
+	}
+
+	public void setUsername(String username) {
+		Username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+
+}
