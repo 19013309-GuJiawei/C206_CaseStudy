1299d890954b2d8af92192f73d62788ba3b9b8f7
19030247 <19030247@DESKTOP-BLGJGSF>
2020-08-18 10:58:52 +0800

    ManageQuotation Class

---
+++ C206_CaseStudy/src/Quotation.java
@@ -0,0 +1,50 @@
+
+public class Quotation {
+	
+	//Alyssa
+	private int requestID;
+	private int quotationID;
+	private String renoCategory;
+	private String description;
+	private String designerName;
+	private String startDate;
+	private double totalAmount;
+	
+	public Quotation(int requestID, int quotationID, String renoCategory, String description, String designerName, String startDate, double totalAmount) {
+		this.requestID = requestID;
+		this.quotationID = quotationID;
+		this.renoCategory = renoCategory;
+		this.description = description;
+		this.designerName = designerName;
+		this.startDate = startDate;
+		this.totalAmount = totalAmount;
+	}
+	
+	public int getRequestID() {
+		return requestID;
+	}
+	
+	public int getQuotationID() {
+		return quotationID;
+	}
+	
+	public String getRenovationCategory() {
+		return renoCategory;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public String getDesignerName() {
+		return designerName;
+	}
+	
+	public String getEarliestStartDate() {
+		return startDate;
+	}
+	
+	public double getTotalQuoteAmount() {
+		return totalAmount;
+	}
+}
