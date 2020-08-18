6dd911587ea6aaf89aa547c7cf21fbf264aabb20
19029144 <19029144@DESKTOP-10DON0T>
2020-08-18 12:15:57 +0800

    Updated

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,20 +1,9 @@
 
-import java.util.ArrayList;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-
 public class C206_CaseStudy {
 
 	private static final int OPTION_QUIT = 4;
-	private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("d/MM/yyyy");
 
 	public static void main(String[] args) {
-
-		ArrayList<Package> packageList = new ArrayList<Package>();
-		
-		packageList.add(new Package(1, "This package is OK",  null, null, 100));
-
 		
 		//hello people :D
 		//jiawei
@@ -24,7 +13,6 @@ public class C206_CaseStudy {
 			menu();
 			option = Helper.readInt("Enter an option > ");
 			if (option == 1) {
-				C206_CaseStudy.viewAllPackage(packageList);
 				//View all package
 			}else if (option == 2) {
 				//Login as Customer
@@ -46,17 +34,7 @@ public class C206_CaseStudy {
 				int AdminOption = Helper.readInt("Enter an option > ");
 				if (AdminOption == 1) {
 					//Manage Customer
-					
-				//Jonathan	
 				}else if (AdminOption == 2) {
-					C206_CaseStudy.packageMenu();
-					int PackageOption = Helper.readInt("Enter an option > ");
-					if (PackageOption == 1) {
-						Package pp = inputPackage();
-						C206_CaseStudy.addPackage(packageList, pp);
-					}else if(PackageOption == 2) {
-						C206_CaseStudy.deletePackage(packageList);
-					}
 					//Manage Package
 				}else if (AdminOption == 3) {
 					//Manage Request for Quotation
@@ -108,9 +86,6 @@ public class C206_CaseStudy {
 		System.out.println("5. Manage Appointment");
 	}
 	
-
-
-
 	//-----------------------------------------------------Manage Appointment----------------------------------------
 	//jiawei
 		private static void appointmentMenu() {
@@ -119,13 +94,6 @@ public class C206_CaseStudy {
 			System.out.println("3. Update Appointment");
 			System.out.println("4. Delete Appointment");
 			System.out.println("5. View Appointment");
-
-		}
-		
-	// JOnathan
-		private static void packageMenu() {
-			System.out.println("1. Add Package");
-			System.out.println("2. Remove Package");
 		}
 	
 	//jiawei
@@ -147,72 +115,6 @@ public class C206_CaseStudy {
 		}
 	}
 	
-	
-	
-	//------------------------------------------------------------------------------- MANAGE PACKAGE--------------------------------------------------------------	
-	
-		// JONATHAN
-		
-	// VIEW ALL PACKAGE
-		
-	public static String retrieveAllPackage(ArrayList<Package> packageList) {
-		String output = "";
-		for (int i = 0; i < packageList.size(); i++) {
-
-			output += String.format("%-10d %-30s %-10s %-10s %-20d \n", packageList.get(i).getCode(),
-					packageList.get(i).getDescription(), packageList.get(i).getStart_Date() , packageList.get(i).getEnd_Date() , packageList.get(i).getAmount());
-	
-		}
-		return output;
-	
-	}
-	public static void viewAllPackage(ArrayList<Package> packageList) {
-		C206_CaseStudy.setHeader("Package LIST");
-		String output = String.format("%-10s %-30s %-10s %-10s %-20s\n", "CODE", "DESCRIPTION",
-				"START DATE", "END DATE","AMOUNT");
-		 output += retrieveAllPackage(packageList);	
-		System.out.println(output);
-	}
-	
-	
-// ADD PACKAGE	
-	
-	public static Package inputPackage() {
-		int code = Helper.readInt("Enter code > ");
-		String description = Helper.readString("Enter description > ");
-		String start = Helper.readString("Enter Start Date> ");
-		String end = Helper.readString("Enter End Date> ");
-		LocalDate startdate = LocalDate.parse(start, format);
-		LocalDate enddate = LocalDate.parse(end, format);
-		int amount = Helper.readInt("Enter amount of package> ");
-
-		Package pp = new Package(code, description, startdate, enddate, amount);
-		return pp;
-		
-	}
-	public static void addPackage(ArrayList<Package> packageList, Package pp) {
-		
-		packageList.add(pp);
-		System.out.println("Package added");
-	}
-	
-	
-	
-	
-// DELETE PACKAGE
-	public static void deletePackage(ArrayList<Package> packageList) {
-		int code = Helper.readInt("Enter code to delete> ");
-		
-		for(int i = 0; i < packageList.size(); i++) {
-			if(packageList.get(i).getCode() == code) {
-				packageList.remove(i);
-				System.out.println("Package removed!");
-			}
-		}
-		
-	}
-
 	//---------------------------------------------------------------------------------------------------------------
-
 
 }
