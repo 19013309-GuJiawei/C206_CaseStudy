f15aa734d358f72a7b4470672e0733c8a5d2da7f
19030247 <19030247@DESKTOP-BLGJGSF>
2020-08-23 01:19:40 +0800

    Solved error Junit failure

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -402,7 +402,7 @@ public class C206_CaseStudyTest {
 	//Joshua
 	@Test
 	public void retrieveCustomer() {
-		String output = String.format("%-10s %-20s %-10s %-10s %-20s\n", "NAME", "ROLE", "EMAIL", "PASSWORD", "STATUS");
+		String output = String.format("%-10s %-20s %-10s %-10s %-20s\n", "NAME", "ROLE", "PASSWORD", "EMAIL", "STATUS");
 		addCustomer();
 		assertEquals("checks if its retrieved properly", output, C206_CaseStudy.retrieveAllCustomer(userCustList));
 
